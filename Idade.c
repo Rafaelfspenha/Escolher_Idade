fb4d7c51a30c2955ddb9504110c943c04ea77976
Rafael <68768478+Rafaelfspenha@users.noreply.github.com>
2023-01-19 14:21:58 -0300

    Update Idade.c

--- Idade.c
+++ Idade.c
@@ -4,53 +4,78 @@
 
 void voltar();
 
-int main() {
+int main()
+{
     int idade;
-    
-    while(idade != -1) {
-    
+
+    while (idade != -1)
+    {
+
         voltar();
         printf("Digite sua idade: ");
-        scanf("%d",&idade);
-        
-        if(idade >= 100){
+        scanf("%d", &idade);
+
+        if (idade >= 100)
+        {
             printf("\nVocê é um centenário.\n\n");
-            }else{
-                if(idade >= 65){
-                    printf("\n>> Você é um idoso.\n\n");
-                    }else{
-                    if(idade <= 65 && idade >= 30){
-                        printf("\n>> Você é um adulto.\n\n");
-                    }else {
-                        if(idade < 30 && idade > 19){
-                            printf("\n>> Você é um jovem adulto.\n\n");
-                        }else{
-                            if(idade <= 17 && idade >= 11){
-                                printf("\n>> Você é um adolescente.\n\n");
-                            }else{
-                                if(idade < 11 && idade >= 0){
-                                    printf("\n>> Você é uma criança.\n\n");
-                                }else{
-                                    if(idade <= -1){
-                                        printf("\n>> Você digitou para sair.");
-                                    }else{
-                                        printf("");
-                                    }
+        }
+        else
+        {
+            if (idade >= 65)
+            {
+                printf("\n>> Você é um idoso.\n\n");
+            }
+            else
+            {
+                if (idade <= 65 && idade >= 30)
+                {
+                    printf("\n>> Você é um adulto.\n\n");
+                }
+                else
+                {
+                    if (idade < 30 && idade > 19)
+                    {
+                        printf("\n>> Você é um jovem adulto.\n\n");
+                    }
+                    else
+                    {
+                        if (idade <= 17 && idade >= 11)
+                        {
+                            printf("\n>> Você é um adolescente.\n\n");
+                        }
+                        else
+                        {
+                            if (idade < 11 && idade >= 0)
+                            {
+                                printf("\n>> Você é uma criança.\n\n");
+                            }
+                            else
+                            {
+                                if (idade <= -1)
+                                {
+                                    printf("\n>> Você digitou para sair.");
+                                }
+                                else
+                                {
+                                    printf("");
                                 }
                             }
                         }
                     }
-                 }
+                }
             }
+        }
     }
     return 0;
- }
-    
-void voltar(){
+}
+
+void voltar()
+{
     printf("--------------------------------------");
     printf("\n\tDigite -1 para sair. ");
     printf("\n--------------------------------------\n\n");
 }
+
 
 
     
